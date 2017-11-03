d17267583cd641a366bccd7131aa170dc257de48
Chris Morris <cmorris@ccistudios.com>
2017-11-03 10:15:25 -0400

    added news articles

--- themes/gage/js/menu.js
+++ themes/gage/js/menu.js
@@ -1,9 +1,11 @@
 (function($){
+  var menu_top = 0;
   $(function(){
+    menu_top = $(".layout-top").position().top;
     $(window).on("scroll", scroll);
   });
   function scroll() {
-    if ($(window).scrollTop() >= $(window).height()) {
+    if ($(window).scrollTop() >= menu_top) {
       $("body").addClass("menu-fixed");
     } else {
       $("body").removeClass("menu-fixed");
