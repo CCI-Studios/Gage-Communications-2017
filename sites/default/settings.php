35818a7b6992323f992f723f4d19abf677bd5a8e
Chris Morris <cmorris@ccistudios.com>
2020-04-14 09:23:44 -0400

    setup a redirect for an uploaded file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -24,3 +24,20 @@ if (file_exists($local_settings)) {
   include $local_settings;
 }
 $settings['install_profile'] = 'standard';
+
+// advanced redirects
+// docs: https://pantheon.io/docs/advanced-redirects
+$redirect_targets = array(
+  '/gage-services.pdf' => '/sites/default/files/Gage%20Core%20Service%20List%20April%202020.pdf',
+);
+
+if ( (isset($redirect_targets[ $_SERVER['REQUEST_URI'] ] ) ) && (php_sapi_name() != "cli") ) {
+  echo 'https://'. $_SERVER['HTTP_HOST'] . $redirect_targets[ $_SERVER['REQUEST_URI'] ];
+  header('HTTP/1.0 301 Moved Permanently');
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $redirect_targets[ $_SERVER['REQUEST_URI'] ]);
+
+  if (extension_loaded('newrelic')) {
+    newrelic_name_transaction("redirect");
+  }
+  exit();
+}
